276485b4bcdc58dd061363a47a13b99ccae612f3
Mehmet Alper Duran <aduran@hurriyetemlak.com>
2020-12-12 23:21:34 +0300

    binary tree level order traversal problem solved

---
+++ src/com/education/binaryTree/BinaryTreeLevelOrderTraversal.java
@@ -0,0 +1,55 @@
+package com.education.binaryTree;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BinaryTreeLevelOrderTraversal {
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(
+                3,
+                new TreeNode(9, null, null), new TreeNode(20,
+                new TreeNode(15, null, null), new TreeNode(7, null, null)));
+
+        List<List<Integer>> lists = levelOrder(root);
+
+    }
+
+    static List<List<Integer>> levels = new ArrayList<>();
+
+    public static void helper(TreeNode node, int level) {
+        if (levels.size() == level)
+            levels.add(new ArrayList<>());
+
+        levels.get(level).add(node.val);
+
+        if (node.left != null)
+            helper(node.left, level + 1);
+        if (node.right != null)
+            helper(node.right, level + 1);
+    }
+
+    public static List<List<Integer>> levelOrder(TreeNode root) {
+        if (root == null) return levels;
+        helper(root, 0);
+        return levels;
+    }
+
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
