46660f3bd84802ffe0950d91602a3453fead6463
Mehmet Alper Duran <aduran@hurriyetemlak.com>
2020-12-18 18:53:41 +0300

    majority element problem solved

---
+++ src/com/education/arrays/Solution.java
@@ -0,0 +1,20 @@
+package com.education.arrays;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
+class MajorityElement {
+    public int majorityElement(int[] nums) {
+
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int num : nums) {
+            if (map.containsKey(num)) {
+                map.put(num, map.get(num) + 1);
+            } else {
+                map.put(num, 1);
+            }
+        }
+        return Collections.max(map.entrySet(), Map.Entry.comparingByValue()).getKey();
+    }
+}
